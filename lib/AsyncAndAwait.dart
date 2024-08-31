7b2c3a376fbe23f8d994ed156485068586f8da8b
techfoon <techfoon.com@gmail.com>
2024-08-31 20:41:41 +0530

    ok

--- lib/AsyncAndAwait.dart
+++ lib/AsyncAndAwait.dart
@@ -1,57 +1,57 @@
-// import 'package:flutter/material.dart';
-// import 'package:shared_preferences/shared_preferences.dart';
+ import 'package:flutter/material.dart';
+ import 'package:shared_preferences/shared_preferences.dart';
 
-// class AsyncAndAwait extends StatefulWidget {
-//   @override
-//   State<AsyncAndAwait> createState() => _AsyncAndAwaitState();
-// }
+ class AsyncAndAwait extends StatefulWidget {
+   @override
+   State<AsyncAndAwait> createState() => _AsyncAndAwaitState();
+ }
 
-// class _AsyncAndAwaitState extends State<AsyncAndAwait> {
-//   String? datavalue;
+ class _AsyncAndAwaitState extends State<AsyncAndAwait> {
+   String? datavalue;
 
-//   ///setDAta
+   //setDAta
 
-//   setData() async {
-//     SharedPreferences setDatapref = await SharedPreferences.getInstance();
+   setData() async {
+     SharedPreferences setDatapref = await SharedPreferences.getInstance();
 
-//     await setDatapref.setString('datavalue', mycontroller.text);
-//   }
+     await setDatapref.setString('datavalue', mycontroller.text);
+   }
 
-//   getData() async {
-//     SharedPreferences getDatapref = await SharedPreferences.getInstance();
+   getData() async {
+     SharedPreferences getDatapref = await SharedPreferences.getInstance();
 
-//     datavalue = getDatapref.getString('datavalue');
-//     setState(() {});
-//   }
+     datavalue = getDatapref.getString('datavalue');
+     setState(() {});
+   }
 
-//   //setData
-//   TextEditingController mycontroller = TextEditingController();
-//   @override
-//   void initState() {
-//     getData();
-//     super.initState();
-//   }
+   //setData
+   TextEditingController mycontroller = TextEditingController();
+   @override
+   void initState() {
+     getData();
+     super.initState();
+   }
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       body: Column(
-//         mainAxisAlignment: MainAxisAlignment.center,
-//         children: [
-//           Text("$datavalue"),
-//           TextField(
-//             controller: mycontroller,
-//           ),
-//           ElevatedButton(
-//               onPressed: () async {
-//                 await setData();
-//                 print(mycontroller.text.toString());
+   @override
+   Widget build(BuildContext context) {
+     return Scaffold(
+       body: Column(
+         mainAxisAlignment: MainAxisAlignment.center,
+         children: [
+           Text("$datavalue"),
+           TextField(
+             controller: mycontroller,
+           ),
+           ElevatedButton(
+               onPressed: () async {
+                 await setData();
+                 print(mycontroller.text.toString());
 
-//                 // await getData();
-//               },
-//               child: Text("click here"))
-//         ],
-//       ),
-//     );
-//   }
-// }
+                  await getData();
+               },
+               child: Text("click here"))
+         ],
+       ),
+     );
+   }
+ }
